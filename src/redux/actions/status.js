3085c48170cd1d016426d1fed5ae8f56dedd088d
FreemenL <liuxiajiang818@163.com>
2018-04-25 13:59:25 +0800

    update

--- src/redux/actions/status.js
+++ src/redux/actions/status.js
@@ -3,7 +3,7 @@ import fetch from 'isomorphic-fetch';
 
 export const getData = (fetchTitle,input) => (dispatch,getState)=>{
   dispatch({type:fetchTitle+'_START'});
-  fetch('https://jsonplaceholder.typicode.com/users',{phone:input})
+  fetch('https://jsonplaceholder.typicode.com/users')
   .then(function(data){
     return data.json();
   })
